6cf99006c94d4da1760e6a5a02182b8ad114628a
Aditya Anand <aditechdevofficial@gmail.com>
2023-07-03 22:45:16 +0530

    :fire: List of video files

---
+++ lib/bloc/video_file_bloc.dart
@@ -0,0 +1,17 @@
+class VideoFileBloc {
+
+  
+  final List<String> videoList = [
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+    "https://cdn.videvo.net/videvo_files/video/premium/video0394/large_watermarked/902-1_902-0405-PD2_preview.mp4",
+    "https://cdn.videvo.net/videvo_files/video/premium/getty_109/large_watermarked/istock-977917514_preview.mp4",
+    "https://cdn.videvo.net/videvo_files/video/premium/getty_46/large_watermarked/istock-894730310_preview.mp4",
+    "https://media.istockphoto.com/id/1387270002/video/hiker-walks-in-wintery-forest-at-night.mp4?s=mp4-640x640-is&k=20&c=vQvQrF57jGGQcsZP7GfN2c9Y5mqzhucndybBXOltV8M=",
+    "https://happy.videvo.net/videvo_files/video/premium/partners0598/large_watermarked/BB_acdb0c30-bc70-4c51-9245-3ee7fad03df5_preview.mp4",
+    "https://happy.videvo.net/videvo_files/video/premium/partners0911/large_watermarked/BB_537e40d9-c6dd-49ca-8541-358a26be42e9_preview.mp4",
+    "https://joy.videvo.net/videvo_files/video/premium/partners0228/large_watermarked/BB_84cd713c-4e89-4728-baae-85f2b1196940_preview.mp4",
+    "https://cdn.videvo.net/videvo_files/video/premium/video0319/large_watermarked/610_610-0114_preview.mp4"
+  ];
+}
+
+VideoFileBloc vib = VideoFileBloc();
