911c382b352b27c2265618f8c41fbd419c37c386
Aditya Anand <aditechdevofficial@gmail.com>
2023-07-03 22:34:35 +0530

    :zap: Page transition added

--- lib/core/theme/my_theme.dart
+++ lib/core/theme/my_theme.dart
@@ -10,8 +10,17 @@ class MyAppTheme {
     return ThemeData(
       primaryColor: th.primaryColor,
       scaffoldBackgroundColor: th.scaffoldBackgroundColor,
+      pageTransitionsTheme: PageTransitionsTheme(
+        builders: {
+          TargetPlatform.android:
+              CustomPageTransitionsBuilder(), // Customize transitions for Android
+          TargetPlatform.iOS:
+              CustomPageTransitionsBuilder(), // Customize transitions for iOS
+        },
+      ),
       appBarTheme: AppBarTheme(
         elevation: 0,
+        centerTitle: true,
         backgroundColor: th.appBackgroundColor,
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarColor: th.statusBarColor,
@@ -23,3 +32,22 @@ class MyAppTheme {
 }
 
 MyAppTheme myAppTheme = MyAppTheme();
+
+class CustomPageTransitionsBuilder extends PageTransitionsBuilder {
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return ScaleTransition(
+      scale: Tween<double>(
+        begin: 0.0,
+        end: 1.0,
+      ).animate(animation),
+      child: child,
+    );
+  }
+}
